b933543dad462e18ae058621e6dffc087710cacf
Herrius <65200014+Herrius@users.noreply.github.com>
2021-09-16 17:58:16 -0500

    google maps configuracion basicas

--- app/src/main/java/com/example/appinfo/Report.java
+++ app/src/main/java/com/example/appinfo/Report.java
@@ -2,7 +2,9 @@ package com.example.appinfo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Report extends AppCompatActivity {
 
@@ -10,5 +12,11 @@ public class Report extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_report);
+    }
+
+    public void GPS(View view) {
+        Intent i = new Intent(getApplicationContext(),MapsActivity.class);
+        startActivity(i);
+
     }
 }
