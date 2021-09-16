1962f59d49a9aaba4ad0c3ebf8b077b66995a21b
Herrius <65200014+Herrius@users.noreply.github.com>
2021-09-16 16:18:07 -0500

    Fin del flujo report y subido de icons

--- app/src/main/java/com/example/appinfo/CategoryReports.java
+++ app/src/main/java/com/example/appinfo/CategoryReports.java
@@ -1,7 +1,10 @@
 package com.example.appinfo;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class CategoryReports extends AppCompatActivity {
 
@@ -9,5 +12,11 @@ public class CategoryReports extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_category_reports);
+
+    }
+
+    public void Report(View view) {
+        Intent i = new Intent(getApplicationContext(),Report.class);
+        startActivity(i);
     }
 }
