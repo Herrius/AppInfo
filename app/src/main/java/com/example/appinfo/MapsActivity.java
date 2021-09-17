26f254f9495428d3edb01068d267ad56a7bb5cd9
Herrius <65200014+Herrius@users.noreply.github.com>
2021-09-17 16:07:08 -0500

    autolocalizacion de google maps

--- app/src/main/java/com/example/appinfo/MapsActivity.java
+++ app/src/main/java/com/example/appinfo/MapsActivity.java
@@ -1,7 +1,11 @@
 package com.example.appinfo;
 
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.FragmentActivity;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.util.Log;
 
@@ -18,7 +22,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
     private GoogleMap mMap;
     private ActivityMapsBinding binding;
-
+    int REQUEST_CODE=200;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,24 +36,21 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mapFragment.getMapAsync(this);
     }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            mMap.setMyLocationEnabled(true);
+            float zoomLevel=18.0f;
+            final LatLng huancayo = new LatLng(-12.068054821014954, -75.21008371397862);
+            mMap.addMarker(new MarkerOptions().position(huancayo).title("Marcar en Huancayo").draggable(true));
+            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(huancayo,zoomLevel));
+        }else {
+            requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION},REQUEST_CODE);
 
-        // Add a marker in Sydney and move the camera
-        float zoomLevel=18.0f;
-        LatLng huancayo = new LatLng(-12.068054821014954, -75.21008371397862);
-        mMap.addMarker(new MarkerOptions().position(huancayo).title("Marcar en Huancayo").draggable(true));
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(huancayo,zoomLevel));
+        }
+
 
 
     }
