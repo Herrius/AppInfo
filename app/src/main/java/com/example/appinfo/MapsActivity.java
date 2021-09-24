ec3ce138e073470903c245c0954e327bc8900e67
Herrius <65200014+Herrius@users.noreply.github.com>
2021-09-24 17:43:12 -0500

    buton guarda la direccion de google maps

--- app/src/main/java/com/example/appinfo/MapsActivity.java
+++ app/src/main/java/com/example/appinfo/MapsActivity.java
@@ -5,11 +5,19 @@ import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.FragmentActivity;
 
 import android.Manifest;
+import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
 import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -20,22 +28,43 @@ import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.example.appinfo.databinding.ActivityMapsBinding;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
 
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, GoogleMap.OnMapClickListener {
+    public Marker ultimaPosicion;
     private GoogleMap mMap;
     private ActivityMapsBinding binding;
+    private Button enviar;
     int REQUEST_CODE=200;
+    private TextView txtlatitud,txtlongitud, txtDireccion;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         binding = ActivityMapsBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-
+        txtlatitud=findViewById(R.id.latitud);
+        txtlongitud=findViewById(R.id.logintud);
+        txtDireccion=findViewById(R.id.direccion);
+        enviar=findViewById(R.id.enviarGPS);
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        enviar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent();
+                intent.putExtra("latitud",ultimaPosicion.getPosition().latitude);
+                intent.putExtra("longitud",ultimaPosicion.getPosition().longitude);
+                intent.putExtra("direccion",txtDireccion.getText());
+                setResult(Activity.RESULT_OK,intent);
+                finish();
+            }
+        });
     }
 
     @RequiresApi(api = Build.VERSION_CODES.M)
@@ -48,9 +77,11 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             mMap = googleMap;
             mMap.setMyLocationEnabled(true);
             float zoomLevel=18.0f;
-            final LatLng huancayo = new LatLng(-12.068054821014954, -75.21008371397862);
-            mMap.addMarker(new MarkerOptions().position(huancayo).title("Marcar en Huancayo").draggable(true));
+            final LatLng huancayo = new LatLng(-12.068054821014954,-75.21008371397862 );
+            ultimaPosicion=mMap.addMarker(new MarkerOptions().position(huancayo).title("Marcar el lugar del problema"));
             mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(huancayo,zoomLevel));
+            mMap.getUiSettings().setZoomControlsEnabled(true);
+            mMap.setOnMapClickListener(this);
         }else {
             finish();
         }
@@ -58,4 +89,25 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
 
     }
+
+
+    @Override
+    public void onMapClick(LatLng latLng) {
+        if(ultimaPosicion !=null){
+            ultimaPosicion.remove();
+        }
+        ultimaPosicion=mMap.addMarker(new MarkerOptions().position(latLng).title("Marcar el lugar del problema"));
+        txtlatitud.setText("Latitud: "+Double.toString(ultimaPosicion.getPosition().latitude));
+        txtlongitud.setText("Longitud: "+Double.toString(ultimaPosicion.getPosition().longitude));
+        Geocoder geocoder=new Geocoder(getApplicationContext(), Locale.getDefault());
+        try{
+            List<Address> direccion=geocoder.getFromLocation(ultimaPosicion.getPosition().latitude,ultimaPosicion.getPosition().longitude,1);
+            txtDireccion.setText(direccion.get(0).getAddressLine(0));
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
+
+
 }
--- app/src/main/java/com/example/appinfo/Report.java
+++ app/src/main/java/com/example/appinfo/Report.java
@@ -2,21 +2,43 @@ package com.example.appinfo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class Report extends AppCompatActivity {
-
-    @Override
+    public static final int REQUEST_CODE_GETMESSAGE=1014;
+    private double latitud,longitud;
+    private String ubicacion;
+    Button direccion;
+        @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_report);
+        direccion=findViewById(R.id.direccion);
     }
 
     public void GPS(View view) {
         Intent i = new Intent(getApplicationContext(),MapsActivity.class);
-        startActivity(i);
+        startActivityForResult(i,REQUEST_CODE_GETMESSAGE);
 
     }
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_CODE_GETMESSAGE) {
+            if (resultCode == Activity.RESULT_OK) {
+                latitud = data.getDoubleExtra("latitud",0);
+                longitud = data.getDoubleExtra("longitud",0);
+                ubicacion = data.getStringExtra("direccion");
+                direccion.setText(ubicacion);
+            } else {
+                Toast.makeText(this, "Ubicacion cancelada", Toast.LENGTH_SHORT).show();
+            }
+        }
+
+    }
+
 }
